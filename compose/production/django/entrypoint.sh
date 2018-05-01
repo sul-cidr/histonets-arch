cfa3bfc210879bbc70596c608412e1ede9b54398
Javier de la Rosa <versae@gmail.com>
2018-05-01 16:22:53 -0700

    Fixes #18. Adding POSTGRES_HOST and POSTGRES_PORT as env vars

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -8,7 +8,13 @@ if [ -z "${POSTGRES_USER}" ]; then
     # the official postgres image uses 'postgres' as default user if not set explictly.
     export POSTGRES_USER=postgres
 fi
-export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}"
+if [ -z "${POSTGRES_HOST}" ]; then
+    export POSTGRES_HOST=postgres
+fi
+if [ -z "${POSTGRES_PORT}" ]; then
+    export POSTGRES_PORT=5432
+fi
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 postgres_ready() {
 python << END
@@ -21,7 +27,8 @@ try:
         dbname="${POSTGRES_DB}",
         user="${POSTGRES_USER}",
         password="${POSTGRES_PASSWORD}",
-        host="postgres"
+        host="${POSTGRES_HOST}",
+        port=int("${POSTGRES_PORT}")
     )
 except psycopg2.OperationalError:
     sys.exit(-1)
